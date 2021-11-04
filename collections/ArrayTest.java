5a3b11045e1617f54fe4a7b75b5cbadda8950fe6
Alexey Martishin <monkey@Alexeys-MacBook-Pro.local>
2021-11-05 00:45:21 +0100

    add sorting for array

--- collections/ArrayTest.java
+++ collections/ArrayTest.java
@@ -27,9 +27,31 @@ public class ArrayTest {
         System.out.println(Arrays.toString(arr));
     }
 
+    private static void sortArray() {
+        int[] arr = {1, 3, 2};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        int[][] brr = {
+            {2, 4},
+            {2, 3},
+            {1, 2}
+        };
+        Arrays.sort(brr, (lhs, rhs) -> {
+            if (lhs[0] == rhs[0]) {
+                return lhs[1] - rhs[1];
+            }
+
+            return lhs[0] - rhs[0];
+        });
+        System.out.println(Arrays.deepToString(brr));
+
+    }
+
     public static void main(String[] args) {
         createArray();
         twoDimensionalArray();
         arrayWithObjects();
+        sortArray();
     }
 }
