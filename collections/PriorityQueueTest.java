fd0e3908eb262cfcbe3c1d49425c500699b99e91
Alexey Martishin <monkey@Alexeys-MacBook-Pro.local>
2021-11-05 01:49:43 +0100

    Priority queue test

---
+++ collections/PriorityQueueTest.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class PriorityQueueTest {
+    private static void createHeap() {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        minHeap.offer(1);
+        minHeap.offer(2);
+        System.out.println(minHeap.peek());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        maxHeap.offer(1);
+        maxHeap.offer(2);
+        System.out.println(maxHeap.peek());
+    }
+
+
+    private static void peekHeap() {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        minHeap.offer(1);
+        minHeap.offer(2);
+        System.out.println(minHeap.peek());
+        minHeap.poll();
+        System.out.println(minHeap.peek());
+    }
+
+    private static void pollHeap() {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        minHeap.offer(1);
+        System.out.println(minHeap.poll());
+        System.out.println(minHeap.isEmpty());
+    }
+
+    public static void main(String[] args) {
+        createHeap();
+        pollHeap();
+    }
+}
